1faffb2504698db9569dd38aedacef8b82eddb2e
Pierangelo <pierangelo.mancusi@gmail.com>
2019-11-18 23:37:22 +0100

    docs: generiamo solo la differenza

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -13,4 +13,22 @@ export class TestComponent implements OnInit {
   //test1
   //test2- fix: aggiunta la condizione per la quale il progetto collassa
   //test3- Fix: aggiunta la condizione per la quale il progetto collassa
+  /*
+  feat: allow provided config object to extend other configs
+
+  BREAKING CHANGE: `extends` key in config file is now used for extending other config files
+
+  out:
+  Bug Fixes
+    aggiunta la condizione per la quale il progetto collassa (82a8f04)
+    main.ts (68a53dc)
+    test changelog (41aedd4)
+  Features
+    allow provided config object to extend other configs (471df41)
+  BREAKING CHANGES
+    extends key in config file is now used for extending other config files
+
+
+  */
+  //test4: docs: generiamo solo la differenza
 }
